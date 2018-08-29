8e9d619a8733b708746bd61eeb13b17c7e0ce31e
kwontaekhwan <taekani@gmail.com>
2018-08-29 18:14:24 +0900

    bed case

--- collect_method.rb
+++ collect_method.rb
@@ -7,9 +7,15 @@ class Array
 end
 
 array = [1,2,3,4]
+array2 = [5,6,7,8]
 
 array.iterate! do |n|
   n**2
 end
 
+array2.iterate! do |n|
+  n**2
+end
+
 puts array.inspect #[1, 4, 9, 16]
+puts array2.inspect #[25, 36, 49, 64]
