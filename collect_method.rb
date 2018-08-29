efab4d0f69189b252421c2a28cd49fe4bbff19ed
kwontaekhwan <taekani@gmail.com>
2018-08-29 18:43:18 +0900

    use method

--- collect_method.rb
+++ collect_method.rb
@@ -8,13 +8,20 @@ end
 
 array = [1,2,3,4]
 array2 = [5,6,7,8]
+array3 = [9,10]
 
 square = Proc.new do |n|
   n**2
 end
 
+def square2(n)
+  n**2
+end
+
 array.iterate!(lambda {|n| n ** 2})
 array2.iterate!(square)
+array3.iterate!(method(:square2))
 
 puts array.inspect #[1, 4, 9, 16]
 puts array2.inspect #[25, 36, 49, 64]
+puts array3.inspect #[25, 36, 49, 64]
