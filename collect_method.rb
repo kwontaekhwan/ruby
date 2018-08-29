42f078327a13f83198b6ba759e7156473c05d47f
kwontaekhwan <taekani@gmail.com>
2018-08-29 18:19:53 +0900

    use Proc

--- collect_method.rb
+++ collect_method.rb
@@ -1,7 +1,7 @@
 class Array
-  def iterate!
+  def iterate!(code)
     self.each_with_index do |n,i|
-      self[i] = yield(n)  # yield(n) 부분에서 n**2를 실행하여 새로운 배열값을 만든다.
+      self[i] = code.call(n)
     end
   end
 end
@@ -9,13 +9,12 @@ end
 array = [1,2,3,4]
 array2 = [5,6,7,8]
 
-array.iterate! do |n|
+square = Proc.new do |n|
   n**2
 end
 
-array2.iterate! do |n|
-  n**2
-end
+array.iterate!(square)
+array2.iterate!(square)
 
 puts array.inspect #[1, 4, 9, 16]
 puts array2.inspect #[25, 36, 49, 64]
